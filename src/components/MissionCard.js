6f413df77a999e824862598b15b35e12c90dd6b7
Pedrin <pedro515vieira@gmail.com>
2022-02-18 19:13:29 -0300

    Finalizando e corrigindo os erros

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -9,7 +9,7 @@ class MissionCard extends Component {
         <p data-testid="mission-name">{ name }</p>
         <p data-testid="mission-year">{ year }</p>
         <p data-testid="mission-country">{ country }</p>
-        <p data-testid="mission-destinantion">{ destination }</p>
+        <p data-testid="mission-destination">{ destination }</p>
       </div>
     );
   }
